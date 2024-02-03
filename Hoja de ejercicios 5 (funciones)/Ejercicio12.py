62849c6c039f2d66e518bec967d716abc8db10d2
perezozo04 <145325345+perezozo04@users.noreply.github.com>
2024-02-03 11:47:11 +0100

    Ejercicio12 terminado

---
+++ Hoja de ejercicios 5 (funciones)/Ejercicio12.py
@@ -0,0 +1,13 @@
+import random
+
+def generadorMatrices():
+    filas = random(2, 6)
+    columnas = random(2, 6)
+    matriz = []
+
+    for _ in range(filas):
+        row = []
+        for _ in range(columnas):
+            column = random(0,100)
+            row.append(column)
+        matriz.append(row)
